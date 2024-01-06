e989370dccd0120a992b6e62d9ef7bdd60164190
Ahmed Hosny <ahmedhosny6688@gmail.com>
2024-01-06 19:32:17 +0200

    Initial commit

---
+++ Sheet #1 (Data type - Conditions)/SayHelloWithC++.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define IO ios::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+
+int main(){
+
+    IO
+
+    cout << "Hello ";
+
+    return 0;
+}
